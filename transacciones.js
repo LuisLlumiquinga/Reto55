c2e42a445e0761d7e77144b799a5a169f5024b90
EDWINCSN <c.edwin32@hotmail.com>
2025-10-15 20:20:07 -0500

    TRANSACCIONES COMPLETO || FUNCIONA OK

--- transacciones.js
+++ transacciones.js
@@ -37,36 +37,76 @@ ejecutarBusqueda = function() {
     let cuentaEncontrada = buscarCuenta(valorNumeroCuenta);
     //Si el resultado es diferente de null, muestra en pantalla, caso contrario muestra un alert
     if (cuentaEncontrada != null) {
-        alert("cuenta encontrada");
+        alert("CUENTA ENCONTRADA");
         habilitarComponente("btnDepositar");
         habilitarComponente("btnRetirar");
         habilitarComponente("txtMonto");
+        mostrarInformacionCuentaEcontrada(cuentaEncontrada);
     } else {
         alert("CUENTA INEXISTENTE");
+        let cmp = document.getElementById("tbInformacion");
+        cmp.innerHTML = "";
     }
 
-}
+};
 
 depositar = function(numeroCuenta, monto) {
     let cuentaAfectada;
     //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
+    cuentaAfectada = buscarCuenta(numeroCuenta);
     //Al saldo actual de la cuenta afectada, le suma el monto que recibe como parámetro
-}
+    cuentaAfectada.saldo += monto;
+    mostrarInformacionCuentaEcontrada(cuentaAfectada);
+};
 
 ejecutarDeposito = function() {
     //Toma el numero de cuenta ingresado en la caja de texto
+    let valorNumeroCuenta = recuperarTexto("txtNumeroCuenta");
     //Toma el monto ingresado en la caja de texto
+    let valorMonto = recuperarFloat("txtMonto");
     //invoca a depositar
+    depositar(valorNumeroCuenta, valorMonto);
     //Muestra un mensaje TRANSACCION EXITOSA
+    alert("TRANSACCION EXITOSA");
     //Muestra en pantalla el nuevo saldo de la cuenta
-}
+};
 
 
 retirar = function(numeroCuenta, monto) {
     let cuentaAfectada;
     //invoca a buscarCuenta, guarda el resultado en la variable cuentaAfectada;
-    //Valida si la cuenta tiene el saldo suficiente para retirar el monto
+    //Valida si la cuenta tiene el saldo suficiente para retirar el monto  
     //Si el saldo es suficiente,al saldo actual de la cuenta afectada, le resta el monto que recibe como parámetro
     //Si el saldo no es suficiente, muestra un alert SALDO INSUFICIENTE
     //Si logra retirar muestra un mensaje TRANSACCION EXITOSA y muestra en pantalla el nuevo saldo de la cuenta
-}
+    cuentaAfectada = buscarCuenta(numeroCuenta);
+    if ((cuentaAfectada.saldo != 0) || (cuentaAfectada.saldo > monto)) {
+        cuentaAfectada.saldo -= monto;
+        alert("TRANSACCION EXITOSA");
+        mostrarInformacionCuentaEcontrada(cuentaAfectada);
+    } else {
+        alert("SALDO INSUFICIENTE");
+    }
+};
+
+ejecutarRetiro = function() {
+    let valorNumeroCuenta = recuperarTexto("txtNumeroCuenta");
+    let valorMonto = recuperarFloat("txtMonto");
+    retirar(valorNumeroCuenta, valorMonto);
+};
+
+//se redunda con esta tabla para mostrar en pantalla la busqueda de cuenta y tambien observar las transacciones actualizadas conforme se apliquen.
+mostrarInformacionCuentaEcontrada = function(objetoEncontrado) {
+    //let cuentaEncontrada = buscarCuenta(objetoEncontrado);
+    let contenidoTabla = "<table>" +
+        "<tr><th>Nro Cuenta</th><th>cedula</th><th>Nombre</th><th>Saldo</th></tr>" +
+        "<tr>" +
+        "<td>" + objetoEncontrado.numeroCuenta + "</td>" +
+        "<td>" + objetoEncontrado.cedula + "</td>" +
+        "<td>" + objetoEncontrado.nombre + " " + objetoEncontrado.apellido + "</td>" +
+        "<td>" + objetoEncontrado.saldo + "</td>" +
+        "<tr>" +
+        "</table>";
+    let cmp = document.getElementById("tbInformacion");
+    cmp.innerHTML = contenidoTabla;
+};
